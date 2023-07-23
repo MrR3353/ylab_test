15c6df2a728cd41dcf6f2208d34a939482527e6f
MrR3353 <mishinstepan2000@gmail.com>
2023-07-24 01:05:33 +0500

    Connecton to postgresql through sqlalchemy

---
+++ models/models.py
@@ -0,0 +1,30 @@
+from sqlalchemy import MetaData, Table, Column, Integer, String, ForeignKey, UUID
+
+metadata = MetaData()
+
+menu = Table(
+    'menu',
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('title', String, nullable=False),
+    Column('description', String, nullable=False),
+)
+
+submenu = Table(
+    'submenu',
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('title', String, nullable=False),
+    Column('description', String, nullable=False),
+    Column('menu_id', Integer, ForeignKey('menu.id', ondelete='CASCADE'))
+)
+
+dish = Table(
+    'dish',
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('title', String, nullable=False),
+    Column('description', String, nullable=False),
+    Column('price', String, nullable=False),
+    Column('submenu_id', Integer, ForeignKey('submenu.id', ondelete='CASCADE'))
+)
