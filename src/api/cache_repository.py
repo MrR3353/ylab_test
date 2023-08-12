3da147a1ce7e20e95310a65306b35693e62a8171
MrR3353 <mishinstepan2000@gmail.com>
2023-08-12 06:19:44 +0500

    Fix default value for args in cache repo methods 0 -> None

--- src/api/cache_repository.py
+++ src/api/cache_repository.py
@@ -16,7 +16,8 @@ class CacheEntity(Enum):
     all = 'm:*'
 
 
-def cached(key: CacheEntity, menu_id: int = 0, submenu_id: int = 0, dish_id: int = 0):  # caching get responses, getting or setting key
+# caching get responses, getting or setting key
+def cached(key: CacheEntity, menu_id: int | None = None, submenu_id: int | None = None, dish_id: int | None = None):
     def inner(func):
         @functools.wraps(func)
         async def wrapper(*args, **kwargs):
@@ -36,54 +37,54 @@ class CacheRepository:
         self.redis = RedisCache()
 
     @switch
-    async def get(self, key: CacheEntity, menu_id: int = 0, submenu_id: int = 0, dish_id: int = 0):
+    async def get(self, key: CacheEntity, menu_id: int | None = None, submenu_id: int | None = None, dish_id: int | None = None):
         if key == CacheEntity.menu_list:
             return await self.redis.get('m:')
         else:
-            assert menu_id
+            assert menu_id is not None
             if key == CacheEntity.menu:
                 return await self.redis.get(f'm:{menu_id}:')
             elif key == CacheEntity.submenu_list:
                 return await self.redis.get(f'm:{menu_id}:s:')
             else:
-                assert submenu_id
+                assert submenu_id is not None
                 if key == CacheEntity.submenu:
                     return await self.redis.get(f'm:{menu_id}:s:{submenu_id}:')
                 elif key == CacheEntity.dish_list:
                     return await self.redis.get(f'm:{menu_id}:s:{submenu_id}:d:')
                 elif key == CacheEntity.dish:
-                    assert dish_id
+                    assert dish_id is not None
                     return await self.redis.get(f'm:{menu_id}:s:{submenu_id}:d:{dish_id}:')
 
     @switch
-    async def set(self, key: CacheEntity, value, menu_id: int = 0, submenu_id: int = 0, dish_id: int = 0):
+    async def set(self, key: CacheEntity, value, menu_id: int | None = None, submenu_id: int | None = None, dish_id: int | None = None):
         if key == CacheEntity.menu_list:
             await self.redis.set('m:', value)
         else:
-            assert menu_id
+            assert menu_id is not None
             if key == CacheEntity.menu:
                 await self.redis.set(f'm:{menu_id}:', value)
             elif key == CacheEntity.submenu_list:
                 return await self.redis.set(f'm:{menu_id}:s:', value)
             else:
-                assert submenu_id
+                assert submenu_id is not None
                 if key == CacheEntity.submenu:
                     await self.redis.set(f'm:{menu_id}:s:{submenu_id}:', value)
                 elif key == CacheEntity.dish_list:
                     return await self.redis.set(f'm:{menu_id}:s:{submenu_id}:d:', value)
                 elif key == CacheEntity.dish:
-                    assert dish_id
+                    assert dish_id is not None
                     await self.redis.set(f'm:{menu_id}:s:{submenu_id}:d:{dish_id}:', value)
 
     @switch
-    async def delete(self, *args: CacheEntity, menu_id: int = 0, submenu_id: int = 0, dish_id: int = 0):
+    async def delete(self, *args: CacheEntity, menu_id: int | None = None, submenu_id: int | None = None, dish_id: int | None = None):
         for arg in args:
             if arg == CacheEntity.menu_list:
                 await self.redis.delete('m:')
             elif arg == CacheEntity.all:
                 await self.redis.delete('m:', is_pattern=True)
             else:
-                assert menu_id
+                assert menu_id is not None
                 if arg == CacheEntity.menu:
                     await self.redis.delete(f'm:{menu_id}:')
                 elif arg == CacheEntity.menu_cascade:
@@ -91,7 +92,7 @@ class CacheRepository:
                 elif arg == CacheEntity.submenu_list:
                     await self.redis.delete(f'm:{menu_id}:s:')
                 else:
-                    assert submenu_id
+                    assert submenu_id is not None
                     if arg == CacheEntity.submenu:
                         await self.redis.delete(f'm:{menu_id}:s:{submenu_id}:')
                     elif arg == CacheEntity.dish_list:
@@ -99,6 +100,6 @@ class CacheRepository:
                     elif arg == CacheEntity.submenu_cascade:
                         await self.redis.delete(f'm:{menu_id}:s:{submenu_id}:', is_pattern=True)
                     else:
-                        assert dish_id
+                        assert dish_id is not None
                         if arg == CacheEntity.dish:
                             await self.redis.delete(f'm:{menu_id}:s:{submenu_id}:d:{dish_id}:')
